fe8d372667f610dc2d7b6843dc992c74725df536
Erik LaBianca <erik.labianca@gmail.com>
2011-07-22 00:33:03 -0400

    implement restoredb command

--- django_extensions/management/commands/restoredb.py
+++ django_extensions/management/commands/restoredb.py
@@ -1,2 +1,57 @@
 __author__ = 'erik'
-  
+
+"""
+ Command for restoring a database
+"""
+
+import os, time
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Backup database. Only Mysql and Postgresql engines are implemented"
+
+    def handle(self, *args, **options):
+        from django.db import connection
+        from ... import settings
+
+        if not os.path.isdir(settings.BACKUP_LOCATION):
+            os.makedirs(settings.BACKUP_LOCATION)
+        infile = os.path.join(settings.BACKUP_LOCATION, "%s.sql" %(settings.BACKUP_BASENAME))
+
+        if 'mysql' in settings.DB_ENGINE:
+            print 'Doing Mysql restore of database %s from %s' % (settings.DB_NAME, infile)
+            self.do_mysql_restore(infile)
+        elif 'postgres' in settings.DB_ENGINE:
+            print 'Doing Postgresql restore of database %s from %s' % (settings.DB_NAME, infile)
+            self.do_postgresql_restore(infile)
+        else:
+            print 'Backup in %s engine not implemented' % settings.DB_ENGINE
+
+    def do_mysql_restore(self, infile):
+        from ... import settings
+        args = []
+        if settings.DB_USER:
+            args += ["--user=%s" % settings.DB_USER]
+        if settings.DB_PASSWD:
+            args += ["--password=%s" % settings.DB_PASSWD]
+        if settings.DB_HOST:
+            args += ["--host=%s" % settings.DB_HOST]
+        if settings.DB_PORT:
+            args += ["--port=%s" % settings.DB_PORT]
+        args += [settings.DB_NAME]
+
+        os.system('mysql %s < %s' % (' '.join(args), infile))
+
+    def do_postgresql_restore(self, infile):
+        from ... import settings
+        args = []
+        if settings.DB_USER:
+            args += ["--username=%s" % settings.DB_USER]
+        if settings.DB_HOST:
+            args += ["--host=%s" % settings.DB_HOST]
+        if settings.DB_PORT:
+            args += ["--port=%s" % settings.DB_PORT]
+        if settings.DB_NAME:
+            args += [settings.DB_NAME]
+        os.system('PGPASSWORD=%s psql -c "drop schema public cascade; create schema public;" %s' % (settings.DB_PASSWD, ' '.join(args)))
+        os.system('PGPASSWORD=%s psql %s < %s' % (settings.DB_PASSWD, ' '.join(args), infile))
